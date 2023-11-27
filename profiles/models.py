c93153b7c7de8e3c128b9851140a39859bdabbe2
isabellaobrien <bellaobrien.it@gmail.com>
2023-11-27 20:38:35 +0000

    add serializer and list view

--- profiles/models.py
+++ profiles/models.py
@@ -7,6 +7,7 @@ class Profile(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
+    content = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/', default='../default_profile_tl2tw0'
     )
