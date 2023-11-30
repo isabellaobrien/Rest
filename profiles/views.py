d8d86d5d61e5b668823ce9ef8815eff5a91a16f7
isabellaobrien <bellaobrien.it@gmail.com>
2023-11-30 19:36:13 +0000

    annotating querysets

--- profiles/views.py
+++ profiles/views.py
@@ -2,7 +2,8 @@
 # from rest_framework import status
 # from rest_framework.views import APIView
 # from rest_framework.response import Response
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from .models import Profile
 from .serializers import ProfileSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
@@ -44,10 +45,28 @@ from drf_api.permissions import IsOwnerOrReadOnly
 #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ProfileList(generics.ListAPIView):
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
