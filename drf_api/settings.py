18788fd21cc0eb152a97b51cb7e970027101b2f0
isabellaobrien <bellaobrien.it@gmail.com>
2023-12-07 20:36:46 +0000

    debug

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -58,7 +58,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['8000-isabellaobrien-rest-os6pc1bw49l.ws-eu106.gitpod.io']
 # ALLOWED_HOSTS = ['localhost', 'django-rest-framework-walkt-edc139f1a895.herokuapp.com']
